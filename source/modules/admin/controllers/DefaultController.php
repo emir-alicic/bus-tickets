f39b0d9074e64d725576e81029e7b59ddb36ef18
Emir Aličić <alicic.emir@hotmail.com>
2015-01-25 22:14:28 +0100

    Bazični login - bez pristupa bazi, totalno simple

--- source/modules/admin/controllers/DefaultController.php
+++ source/modules/admin/controllers/DefaultController.php
@@ -2,12 +2,38 @@
 
 namespace app\modules\admin\controllers;
 
+use Yii;
 use yii\web\Controller;
+use app\models\LoginForm;
 
 class DefaultController extends Controller
 {
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    public function actionLogin()
+    {
+        if (!\Yii::$app->user->isGuest){
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack(Yii::$app->urlManager->createUrl(['/admin']));
+        } else {
+            return $this->render('login', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
     }
 }
